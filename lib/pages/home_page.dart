8ead052ca0a9c3885e6e6bfea13273e526863cb7
Jonathan Setiawan <jonathanrustam2@gmail.com>
2025-02-07 00:24:40 +0900

    update

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[200],
+      appBar: AppBar(
+        title: Text('TO DO'),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          TodoTile(),
+        ],
+      )
+    );
+  }
+}
