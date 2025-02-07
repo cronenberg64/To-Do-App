9cdead1a328ba4c52d14e1eda2e72fafb844b801
Jonathan Setiawan <jonathanrustam2@gmail.com>
2025-02-07 23:01:29 +0900

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,19 +9,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // list of todo tasks
+  List toDoList = [
+    ["Task 1", false],
+    ["Task 2", false],
+    ["Task 3", false],
+    ["Task 4", false],
+    ["Task 5", false],
+    ["Task 6", false],
+    ["Task 7", false],
+    ["Task 8", false],
+    ["Task 9", false],
+    ["Task 10", false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[200],
-      appBar: AppBar(
-        title: Text('TO DO'),
-        elevation: 0,
-      ),
-      body: ListView(
-        children: [
-          TodoTile(),
-        ],
-      )
-    );
+        backgroundColor: Colors.green[200],
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('TO DO'),
+          elevation: 0,
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (context, index) {
+            return TodoTile(
+              taskName: toDoList[index][0],
+              isDone: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index),
+            );
+          },
+        ));
   }
 }
--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
 class TodoTile extends StatelessWidget {
-  const TodoTile({super.key});
+
+  final String taskName;
+  final bool isDone;
+  final Function(bool?)? onChanged;
+
+  const TodoTile({
+    super.key, 
+    required this.taskName, 
+    required this.isDone, 
+    this.onChanged
+    });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.all(25),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: [
+            // checkbox
+            Checkbox(value: isDone, onChanged: onChanged),
+          
+            // task name
+            Text(taskName)],
+        ),
+        decoration: BoxDecoration(
+        color: Colors.green[100],
+        borderRadius: BorderRadius.circular(10),
+      ),
+    ));
   }
 }
